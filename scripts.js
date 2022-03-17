a43b1317f7e9cc1f1663557b0c2368f61a09766d
AndrewDamas <damas.andrew@gmail.com>
2022-03-16 21:26:25 -0400

    animation

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.querySelector(".menubuttons").addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(".foodchoices").classList.toggle("foodchoicesshow");
+})
+
