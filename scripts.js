82fc33f69fc202e7cbe5eb6bf5ddbb2396f52529
AndrewDamas <damas.andrew@gmail.com>
2022-03-16 21:38:51 -0400

    close not working

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 document.querySelector(".menubuttons").addEventListener("click", function(e) {
     e.preventDefault();
     document.querySelector(".foodchoices").classList.toggle("foodchoicesshow");
+    document.querySelector(".foodchoices").classList.toggle("foodchoicesclose");
 })
 
