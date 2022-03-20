abd3add47b15828a338c0760a1917fad7930541b
ajako19 <jakepereira2004@gmail.com>
2022-03-20 14:46:45 -0400

    yo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
-document.querySelector(".menubuttons").addEventListener("click", function(e) {
+
+
+
+// menubuttons.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     document.querySelector(".foodchoices").classList.toggle("foodchoicesshow");
+
+// })
+
+
+let menubuttons = document.querySelector("form");
+
+menubuttons.forEach(addEventListener("click", function(e) {
     e.preventDefault();
     document.querySelector(".foodchoices").classList.toggle("foodchoicesshow");
-    document.querySelector(".foodchoices").classList.toggle("foodchoicesclose");
-})
+    
+
+}));
 
