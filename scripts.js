85bc59a67512f61d7ea461ba486a0002868897f5
ajako19 <jakepereira2004@gmail.com>
2022-03-24 18:53:47 -0400

    javascript

--- scripts.js
+++ scripts.js
@@ -4,7 +4,8 @@ let products = [
         "category": "Entree",
         "description": "This is a hamburger",
         "price": 11.95,
-        'pictureLink': './images/hamburger.png'
+        'pictureLink': './images/hamburger.png',
+        "inCart": 0,
         
     },
     {
@@ -12,63 +13,72 @@ let products = [
         "category": "Entree",
         "description": "This is ramen",
         "price": 14.95,
-        'pictureLink': './images/ramen.png'
+        'pictureLink': './images/ramen.png',
+        "inCart": 0,
     },
     {
         "name": "Caesar Salad",
         "category": "Salads",
         "description": "This is a caesar salad",
         "price": 9.95,
-        'pictureLink': './images/'
+        'pictureLink': './images/caesersalad.png',
+        "inCart": 0,
     },
     {
         "name": "Greek Salad",
         "category": "Salads",
         "description": "This is a greek salad",
         "price": 9.95,
-        'pictureLink': './images/'
+        'pictureLink': './images/greeksalad.png',
+        "inCart": 0,
     },
     {
         "name": "Dumplings",
         "category": "Sides",
         "description": "5 dumplings",
         "price": 7.50,
-        'pictureLink': './images/'
+        'pictureLink': './images/dumplings.png',
+        "inCart": 0,
     },
     {
         "name": "Fries",
         "category": "Sides",
         "description": "These are fries",
         "price": 4.25,
-        'pictureLink': './images/'
+        'pictureLink': './images/fries.png',
+        "inCart": 0,
     },
     {
         "name": "Coke",
         "category": "Drinks",
         "description": "This is a coke",
         "price": 2.25,
-        'pictureLink': './images/'
+        'pictureLink': './images/coke.png',
+        "inCart": 0,
     },
     {
         "name": "Ramune",
         "category": "Drinks",
         "description": "This is a pop",
         "price": 2.65,
-        'pictureLink': './images/'
+        'pictureLink': './images/ramune.png',
+        "inCart": 0,
     },
     {
         "name": "Mochi",
         "category": "Dessert",
         "description": "3 Mochis",
         "price": 5.25,
-        'pictureLink': './images/'
+        'pictureLink': './images/mochi.png',
+        "inCart": 0,
     },
     {
         "name": "Puppy Chow",
         "category": "Dessert",
         "description": "This is puppy chow",
         "price": 5.95,
-        'pictureLink': './images/'
+        'pictureLink': './images/puppychow.png',
+        "inCart": 0,
     },
 ]
 
@@ -98,3 +108,47 @@ menubuttons.forEach(function(buttons){
         buttons.nextElementSibling.classList.toggle("show");
     });
 });
+
+let cartbuttons = document.querySelectorAll(".cartbutton");
+
+for (let i = 0; i < cartbuttons.length; i++) {
+    cartbuttons[i].addEventListener("click", (e) => {
+        e.preventDefault();
+        shownProduct(products[i]);
+        let itemcheckout = document.createElement("div")
+        itemcheckout.classList.add("itemcheckout")
+        document.querySelector(".form2").append(itemcheckout)
+        
+        let namecheckout = document.createElement("h3");
+        namecheckout.innerText = `${products[i].name} | $${products[i].price}`;
+        itemcheckout.appendChild(namecheckout);
+        
+        let image = document.createElement("img");
+        image.setAttribute("src", products[i].pictureLink);
+        image.classList.add("imagecheckout");
+        itemcheckout.append(image);
+        
+        let quantitycheckout = document.createElement("div");
+        quantitycheckout.innerText = `Quantity`;
+        itemcheckout.append(quantitycheckout);
+
+        let exitbutton = document.createElement("button");
+        exitbutton.classList.add("exitbutton")
+        itemcheckout.appendChild(exitbutton)
+
+        console.log(products[i].price)
+    })
+}
+
+function shownProduct(product) {
+    console.log("My product is", product)
+}
+
+
+
+// cartbuttons.forEach(function(buttons){
+//     buttons.addEventListener("click", function(e){
+//         e.preventDefault();
+//     });
+// });
+
